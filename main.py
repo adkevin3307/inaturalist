4700a0459a8adda4235d446d01115f0a6131358d
adkevin3307 <adkevin3307@gmail.com>
2020-10-03 23:06:58 +0800

    update gitignore, use resnet

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ from torchvision import models, transforms
 
 from Model import Model, EarlyStopping
 from autoaugment import ImageNetPolicy
-from efficientnet_pytorch import EfficientNet
 from InaturalistDataset import InaturalistDataset
 
 
@@ -22,10 +21,12 @@ def load_data(image_size, category_filter, train_test_split):
         transforms.RandomHorizontalFlip(),
         transforms.RandomAffine(30, translate=(0.3, 0.3), scale=(1.0, 1.5)),
         ImageNetPolicy(),
+        # transforms.Grayscale(3),
         transforms.ToTensor()
     ])
     test_transform = transforms.Compose([
         transforms.Resize(image_size),
+        # transforms.Grayscale(3),
         transforms.ToTensor()
     ])
 
@@ -65,33 +66,26 @@ def load_data(image_size, category_filter, train_test_split):
     return (train_loader, val_loader, test_loader)
 
 
-def load_net(output_classes, model_name, net=None):
+def load_net(output_classes, net=None):
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-    if model_name == 'resnet':
-        if net == None:
-            net = models.resnet18(pretrained=True)
-            net.fc = nn.Sequential(
-                nn.Linear(net.fc.in_features, 512),
-                nn.ReLU(),
-                nn.Dropout(0.5),
-                nn.Linear(512, output_classes)
-            )
-        else:
-            net.fc[-1] == nn.Linear(net.fc[-1].in_features, output_classes)
-    elif model_name == 'efficientnet':
-        if net == None:
-            net = EfficientNet.from_pretrained('efficientnet-b0')
-            net._fc = nn.Linear(net._fc.in_features, output_classes)
-        else:
-            net._fc = nn.Linear(net._fc.in_features, output_classes)
-
     if net == None:
+        net = models.resnet18(pretrained=True)
+
         for param in list(net.parameters())[: -15]:
             param.requires_grad = False
+
+        net.fc = nn.Sequential(
+            nn.Linear(net.fc.in_features, 512),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(512, output_classes)
+        )
     else:
-        for param in net.parameters():
-            param.requires_grad = True
+        for param in list(net.parameters())[: -5]:
+            param.requires_grad = False
+
+        net.fc = nn.Linear(net.fc[0].in_features, output_classes)
 
     net = net.to(device)
 
@@ -105,17 +99,17 @@ def run(net, category_filter, train_test_split, patience, moniter, is_query=Fals
     optimizer = torch.optim.Adam(net.parameters(), lr=0.0005, weight_decay=1e-4)
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=4, gamma=0.1)
     criterion = nn.CrossEntropyLoss()
-    early_stopping = EarlyStopping(patience=patience, moniter=moniter)
+    early_stopping = EarlyStopping(patience=patience, moniter=moniter, min_delta=0.0005)
 
     model = Model(net, optimizer, criterion)
-    # model.summary((1, 3) + image_size)
+    model.summary((1, 3) + image_size)
 
     model.train(train_loader, epochs=30, val_loader=val_loader, scheduler=scheduler, early_stopping=early_stopping)
 
     if is_query == False:
         history = model.test(test_loader)
 
-        return (model, history)
+        return history
     else:
         x_test, y_test = [], []
         labels = test_loader.dataset.dataset.labels()
@@ -135,9 +129,10 @@ def run(net, category_filter, train_test_split, patience, moniter, is_query=Fals
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-m', '--model', dest='model_name', type=str, default='resnet')
     parser.add_argument('-c', '--config', dest='config_name', type=str, default='config.yaml')
     args = parser.parse_args()
+
+    name, extension = os.path.splitext(os.path.basename(args.config_name))
 
     logger = logging.getLogger('Processing Logger')
     logger.setLevel(logging.DEBUG)
@@ -145,7 +140,7 @@ if __name__ == '__main__':
     log_stream = logging.StreamHandler()
     log_stream.setLevel(logging.DEBUG)
 
-    log_file = logging.FileHandler('process.log')
+    log_file = logging.FileHandler(f'{name}.log')
     log_file.setLevel(logging.DEBUG)
 
     formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
@@ -160,26 +155,22 @@ if __name__ == '__main__':
 
     image_size = (240, 240)
 
-    with open(args.config_name, 'r') as config_file:
-        config = yaml.load(config_file, Loader=yaml.FullLoader)
-
-    net = load_net(len(config['train']), args.model_name)
-
-    model, history = run(net, config['train'], train_test_split=0.75, patience=7, moniter='val_loss')
-
     if not os.path.exists('backup'):
         os.makedirs('backup')
 
-    name, extension = os.path.splitext(os.path.basename(args.config_name))
-    model.export(os.path.join('backup', name + '.onnx'), ((1, 3) + image_size))
+    with open(args.config_name, 'r') as config_file:
+        config = yaml.load(config_file, Loader=yaml.FullLoader)
+
+    net = load_net(len(config['train']))
+
+    history = run(net, config['train'], train_test_split=0.75, patience=5, moniter='val_loss')
 
     logger.info(f'train accuracy: {(history["accuracy"]):.3f}')
 
     total = 0
     correct = 0
-
-    for category_filter in config['test']:
-        net_copy = load_net(len(category_filter['class']), args.model_name, net=copy.deepcopy(net))
+    for task, category_filter in enumerate(config['test']):
+        net_copy = load_net(len(category_filter['class']), net=copy.deepcopy(net))
 
         train_test_split = {}
         for i in range(len(category_filter['class'])):
@@ -187,12 +178,14 @@ if __name__ == '__main__':
 
         logger.info(f'train test split: {train_test_split}')
 
-        y_test, y_hat = run(net_copy, category_filter['class'], train_test_split=train_test_split, patience=4, moniter='loss', is_query=True)
+        y_test, y_hat = run(net_copy, category_filter['class'], train_test_split=train_test_split, patience=3, moniter='loss', is_query=True)
 
         total += len(y_test)
         correct += (y_test == y_hat).sum()
 
         logger.info(f'\n{classification_report(y_test, y_hat)}')
+
+        torch.save(net_copy, f'backup/{name}_model_{(task + 1)}.pt')
 
     logger.info(f'average accuracy: {(correct / total):.3f}')
 
